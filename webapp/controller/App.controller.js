9274c0b0a0eacfbdf56a3866851ef7f0f69081c4
Weichun TANG <weichun@My-MacBook-Pro-16.local>
2023-06-01 16:38:21 +0800

    yea

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -3,6 +3,11 @@ sap.ui.define([
 ], function (Controller) {
     "use strict";
     return Controller.extend("sap.ui.demo.controller.App", {
-        incrementBy1 : function () {}
+        incrementBy1 : function () {
+            let myTextElement = this.getView().byId("counter");
+            let myNum = parseInt(myTextElement.getText());
+            let myNewNum = myNum + 1;
+            myTextElement.setText(myNewNum);
+        }
     });
 });
