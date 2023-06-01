b77d9e5e63eefeb49cdaf85573ecfc1a0c771986
Weichun TANG <weichun@My-MacBook-Pro-16.local>
2023-06-01 16:58:01 +0800

    Correct path of view

--- webapp/index.js
+++ webapp/index.js
@@ -1,14 +1,15 @@
 sap.ui.define([
     "sap/m/Button",
-], function (Button, XMLView) {
+    "sap/ui/core/mvc/XMLView"
+], function (XMLView) {
     "use strict";
 
-    new Button({
-        text: "Hello World",
-    }).placeAt("content");
+    // new Button({
+    //     text: "Hello World",
+    // }).placeAt("content");
 
     XMLView.create({
-        viewName: "sap.ui.demo.App"
+        viewName: "sap.ui.demo.view.App"
     }).then(function (oView) {
         oView.placeAt("content");
     });
