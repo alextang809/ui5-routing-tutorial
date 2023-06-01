54a5126afc2d7e8723bd991b214b4ea064e7a9b0
Weichun TANG <weichun@My-MacBook-Pro-16.local>
2023-06-01 16:09:18 +0800

    Add XMLView to js, create instance for XML

--- webapp/index.js
+++ webapp/index.js
@@ -1,9 +1,15 @@
 sap.ui.define([
     "sap/m/Button",
-], function (Button) {
+], function (Button, XMLView) {
     "use strict";
 
     new Button({
         text: "Hello World",
     }).placeAt("content");
+
+    XMLView.create({
+        viewName: "sap.ui.demo.App"
+    }).then(function (oView) {
+        oView.placeAt("content");
+    });
 });
